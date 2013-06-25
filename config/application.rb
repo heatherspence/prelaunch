e827edf7d6f7a2c2a10ceb018c064ebdb9aa9877
Heather Spence <heather@fake.com>
2013-06-25 10:04:52 -0400

    background image - larry's suggestion

--- config/application.rb
+++ config/application.rb
@@ -78,5 +78,6 @@ module Prelaunch
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.1'
+    config.assets.initialize_on_precompile = false
   end
 end
